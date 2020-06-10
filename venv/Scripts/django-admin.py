18ee0c23bdfb5c42f8cfe8dde4ee75c3cb26cc5c
João Neves <joao.neves@enecad.com.br>
2020-06-10 14:10:49 -0300

    repositorio pronto

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\joaoneves\source\repos\semana5codenation\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
